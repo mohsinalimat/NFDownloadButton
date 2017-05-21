d39922063b4fc5ae32d3890205280e0087d2d5c0
Leonardo <leo@leocardz.com>
2017-05-22 00:24:14 +0200

    🚀

---
+++ Sources/DownloadButton.h
@@ -0,0 +1,19 @@
+//
+//  DownloadButton.h
+//  DownloadButton
+//
+//  Created by Leonardo Cardoso on 21/05/2017.
+//  Copyright © 2017 leocardz.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DownloadButton.
+FOUNDATION_EXPORT double DownloadButtonVersionNumber;
+
+//! Project version string for DownloadButton.
+FOUNDATION_EXPORT const unsigned char DownloadButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DownloadButton/PublicHeader.h>
+
+
